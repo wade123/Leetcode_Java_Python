744036f97989146dfb9bbbf7ab33b1316eeaa131
tyu <yutingshan@gmail.com>
2014-12-19 12:20:57 -0600

    modify

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,14 +1,14 @@
 class Solution:
-    # @param s, a string
-    # @return an integer
-    def lengthOfLastWord(self, s):
-        if s == '': return 0
-        k = len(s)
-        i = k-1
-        while s[i] == ' ' and i >= 0:
-            i -= 1
-        le = 0
-        while s[i] != ' ' and i >= 0:
-            le += 1
-            i -= 1
-        return le
+	# @param s, a string
+    	# @return an integer
+    	def lengthOfLastWord(self, s):
+        	if s == '': 
+			return 0
+		i = len(s) - 1
+        	while s[i] == ' ' and i >= 0:
+			i -= 1
+        	le = 0
+        	while s[i] != ' ' and i >= 0:
+			le += 1
+            		i -= 1
+        	return le
