a9d629c7c0626189d828f69b9c0bfb4f746855e1
tyu <yutingshan@gmail.com>
2014-12-21 11:42:57 -0600

    2pointers array

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,23 @@
+'''Version 1'''
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        if len(A) <= 1: return len(A)
+        j = 0
+        for i in xrange(len(A)):
+            if A[i] == A[j]:
+                continue
+            else:
+                j += 1
+                A[j] = A[i]
+        return j + 1
+
+'''Version 2'''
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        A = list(set(A))
+        return len(A)
+        
