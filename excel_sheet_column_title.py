993ac75a276c6511d6cb19a59b83c562a78a76cc
tyu <yutingshan@gmail.com>
2014-12-22 16:02:48 -0600

    math

--- excel_sheet_column_title.py
+++ excel_sheet_column_title.py
@@ -7,3 +7,11 @@ class Solution:
             res += ch[(num-1) % 26]
             num = (num-1)/26
         return res[::-1]
+
+'''One-line Version''' 
+# NOTE!! this one-line version is from Leetcode discussion, not written by me.
+class Solution:
+    # @return a string
+    def convertToTitle(self, num):
+        return "" if num == 0 else self.convertToTitle((num - 1) / 26) + chr((num - 1) % 26 + ord('A'))
+        
