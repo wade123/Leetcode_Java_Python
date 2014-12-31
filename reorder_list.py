8776d52ff3f59e9348b24b1c120159b06db896a4
tyu <yutingshan@gmail.com>
2014-12-30 23:49:02 -0600

    linked list

--- reorder_list.py
+++ reorder_list.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+'''Version 1'''
 class Solution:
     # @param head, a ListNode
     # @return nothing
@@ -20,3 +20,24 @@ class Solution:
             lis[i].next = lis[le - 1 - i]
             lis[le - 1 - i].next = lis[i + 1]
         lis[le / 2].next = None
+
+'''Version 2''' #Time Limit Exceeded
+class Solution:
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if head == None or head.next == None or head.next.next == None:
+            return
+        h = head
+        while h.next:
+            p = h
+            q = p.next
+            while q.next:
+                p = p.next
+                q = q.next
+            q.next = h.next
+            h.next = q
+            p.next = None
+            if h.next == None or h.next.next == None:
+                return
+            h = h.next.next
