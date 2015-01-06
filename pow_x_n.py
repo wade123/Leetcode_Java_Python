231b125c842bc5c66826e936d48af7e2e0f6b91c
tyu <yutingshan@gmail.com>
2015-01-05 22:50:16 -0600

    binary search, math

---
+++ pow_x_n.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param x, a float
+    # @param n, a integer
+    # @return a float
+    def pow(self, x, n):
+        if n == 0:  return 1
+        return self.powPos(x, n) if n > 0 else 1 / self.powPos(x, -n)
+
+    def powPos(self, x, n):
+        if n == 1:  return x
+        if n % 2 == 0:
+            return self.powPos(x * x, n / 2)
+        if n % 2 == 1:
+            return x * self.powPos(x * x, (n - 1) / 2)
