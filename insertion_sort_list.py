914be90428569e3034488f8f2d8235bb35eb3689
tyu <yutingshan@gmail.com>
2015-01-18 12:30:45 -0600

    sort, linked list

--- insertion_sort_list.py
+++ insertion_sort_list.py
@@ -1,3 +1,4 @@
+'''Version 1'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -26,3 +27,29 @@ class Solution:
                 q.next.next = tem
         return dummy.next
         
+'''Version 2'''
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def insertionSortList(self, head):
+        if not head or head.next == None:  
+            return head
+        
+        lst = []
+        while head:
+            lst.append(head.val)
+            head = head.next
+        
+        for i in xrange(1, len(lst)):
+            temp = lst[i]
+            j = i - 1
+            while j >= 0 and temp < lst[j]:
+                lst[j + 1] = lst[j]
+                j -= 1
+            lst[j + 1] = temp
+        
+        res = [ListNode(i) for i in lst]
+        for i in xrange(len(lst) - 1):
+            res[i].next = res[i + 1]
+        res[len(lst) - 1].next = None
+        return res[0]
