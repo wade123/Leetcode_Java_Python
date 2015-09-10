c2763e5c1e2bf91f9897244bdec6c58f4f79edef
tyu <yutingshan@gmail.com>
2015-09-10 15:43:44 -0500

    Sep 10

--- Leetcode_Java/BestTimeToBuyAndSellStockFour.java
+++ Leetcode_Java/BestTimeToBuyAndSellStockFour.java
@@ -3,6 +3,9 @@ public class BestTimeToBuyAndSellStockFour {
         if (prices == null || prices.length <= 1) {
             return 0;
         }
+        if (k >= prices.length) {
+            return maxProfit2(prices);
+        }
         int length = prices.length;
         int[] local = new int[k + 1];
         int[] global = new int[k + 1];
@@ -14,6 +17,19 @@ public class BestTimeToBuyAndSellStockFour {
             }
         }
         return global[k];
+    }
+
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
